4223caa8f62cec6b65eeda95f5e5d4751b2850da
avfcpieface <44671015+avfcpieface@users.noreply.github.com>
2019-12-05 10:06:35 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   getThemes () {
     return {
-      'plugin-light-mode': {
+      'plugin-Aston-Villa-Theme': {
         cssPath: 'styles/light.css',
         darkMode: false
       }
